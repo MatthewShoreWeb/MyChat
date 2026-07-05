3bb8b2db16d12c568e4d9340340f09100c225dc1
matthewshoreweb <mazza1205@gmail.com>
2026-07-05 20:44:02 +0100

    UI improvements

--- reactchat/src/Components/Chat/ChatContainer.tsx
+++ reactchat/src/Components/Chat/ChatContainer.tsx
@@ -1,19 +1,43 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './Chat.css';
 
-interface EndUser {
+interface types {
     endUser: string
+    sendMessage: Function;
 }
 
-export default function ChatContainer({ endUser }: EndUser) {
+export default function ChatContainer({ endUser, sendMessage }: types) {
+    const messageBox = useRef<HTMLTextAreaElement>(null);
+    const [headerMessage, updateHeader] = useState('');
+
+    useEffect(() => {
+        try {
+            const setMessage = endUser ? `You are talking to ${endUser}` : 'Please select a user to chat with';
+            updateHeader(setMessage);
+        } catch (e) { console.log(e); }
+    }, [endUser]);
+
+    // Wrapper function to perform validation:
+    function sendMessageWrapper() {
+        const userMessage = messageBox?.current?.value;
+        if (typeof userMessage !== 'string' || !userMessage.length || typeof sendMessage !== 'function') return;
+
+        sendMessage();
+    }
+
     return (
         <div id='chatContainer'>
             <div id='chatView'>
+                <div id='chatViewHeader'>
+                    <p>{headerMessage}</p>
+                </div>
+                <div id='chatLog'>
 
+                </div>
             </div>
             <div id='chatInput'>
-                <textarea />
-                <div id='sendButton'>
+                <textarea ref={messageBox} placeholder='Enter a message' />
+                <div id='sendButton' onClick={() => { }}>
                     Send Message
                 </div>
             </div>
